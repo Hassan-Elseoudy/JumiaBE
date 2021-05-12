f9bd096b6ffa2c37c852046ea19ce98c1ae683df
helseoudy <hassan.elseoudy@cegedim.com>
2021-05-12 15:12:32 +0200

    Adding Customer Service

---
+++ src/main/java/com/jumia/exercise/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.jumia.exercise.service;
+
+import com.jumia.exercise.controller.dto.CustomerResponseV1;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<CustomerResponseV1> getMany(Integer countryId, Boolean isValid);
+}
